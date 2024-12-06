ce238d808618bb02c6273a9ecd81237f5d3fa958
imaanick <nickolaytrusov01@gmail.com>
2024-12-06 19:53:52 +0300

    refactor: get_matching_forms

--- app/application/form.py
+++ app/application/form.py
@@ -13,13 +13,20 @@ async def filter_matching_forms(
     return matching_forms
 
 
-async def get_matching_forms(
+async def get_search_fields(
         form_data: dict[str, str],
-        database: DatabaseGateway,
 ) -> list[FormTemplate]:
     search_fields = []
     for name, field_value in form_data.items():
         search_fields.append(FormField(name=name, field={"value": field_value}))
+    return search_fields
+
+
+async def get_matching_forms(
+        form_data: dict[str, str],
+        database: DatabaseGateway,
+) -> list[FormTemplate]:
+    search_fields = await get_search_fields(form_data)
     forms = await database.get_matching_forms(len(search_fields))
     matching_forms = await filter_matching_forms(forms, search_fields)
     return matching_forms
