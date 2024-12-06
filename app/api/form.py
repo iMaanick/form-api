0ba4079150b247552b482146630f007218261631
imaanick <nickolaytrusov01@gmail.com>
2024-12-06 17:22:22 +0300

    refactor: get_form response_model

--- app/api/form.py
+++ app/api/form.py
@@ -1,25 +1,18 @@
-from typing import Any, Annotated
+from typing import Annotated
 
 from fastapi import APIRouter, Body, Depends
 
 from app.application.form import get_matching_forms
+from app.application.models import GetFormResponse, FormTemplate
 from app.application.protocols.database import DatabaseGateway
 
 form_router = APIRouter()
 
 
-@form_router.post("/get_form")
+@form_router.post("/get_form", response_model=list[GetFormResponse])
 async def get_form(
         database: Annotated[DatabaseGateway, Depends()],
         form_data: dict[str, str] = Body(),
-) -> Any:
+) -> list[FormTemplate]:
     forms = await get_matching_forms(form_data, database)
     return forms
-
-# {
-#   "additionalProp1": "string",
-#   "additionalProp2": "+79111111111",
-#   "additionalProp3": "12.12.2001",
-#   "additionalProp4": "2001-12-12",
-#   "additionalProp5": "123@mail.ru"
-# }
--- app/application/models/__init__.py
+++ app/application/models/__init__.py
@@ -5,8 +5,10 @@ __all__ = [
     "Text",
     "FormField",
     "FormTemplate",
+    "GetFormResponse",
 ]
 
 from .field_type import Date, Email, Phone, Text
 from .form_field import FormField
 from .form_template import FormTemplate
+from .get_form_respose import GetFormResponse
