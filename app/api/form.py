da327401aeddaa1c8b0a998d70f95af92bd69d14
imaanick <nickolaytrusov01@gmail.com>
2024-12-07 16:46:16 +0300

    feat: add docstring

--- app/api/form.py
+++ app/api/form.py
@@ -15,6 +15,55 @@ async def get_form(
         database: Annotated[DatabaseGateway, Depends()],
         form_data: dict[str, str] = Body(),
 ) -> Union[list[FormTemplate], dict[str, FieldValueType]]:
+    """
+    Handles POST requests to match a submitted form against predefined form templates.
+
+    **Endpoint**: `/get_form`
+
+    ### Request:
+    - **Method**: POST
+    - **Body**: A dictionary containing field names as keys and their respective values as strings.
+
+    Example:
+    ```json
+    {
+        "Email Address": "user1@example.com",
+        "Phone Number": "+71234567890",
+        "Birth Date": "2000-01-01"
+    }
+    ```
+
+    ### Response:
+    - If a matching template is found:
+      A list of dictionaries containing the names of matching form templates.
+      Example:
+      ```json
+      [
+          {
+              "name": "Registration Form"
+          }
+      ]
+      ```
+    - If no matching template is found:
+      A dictionary with field names as keys and their determined types as values.
+      Example:
+      ```json
+      {
+          "Email Address": "email",
+          "Phone Number": "phone",
+          "Birth Date": "date"
+      }
+      ```
+
+    ### Parameters:
+    - `database` (DatabaseGateway): Injected dependency for database interaction.
+    - `form_data` (dict[str, str]): Submitted form fields and their respective values.
+
+    ### Returns:
+    - `Union[list[FormTemplate], dict[str, FieldValueType]]`:
+      - A list of matching form templates or
+      - A dictionary with field types if no match is found.
+    """
     search_fields = await get_search_fields(form_data)
     forms = await get_matching_forms(search_fields, database)
     if not forms:
